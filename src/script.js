9efa0cc2662c07301c171c37919d1175d88bb18a
JulKuzmi <enchantress000@mail.ru>
2023-06-22 18:52:26 +0300

    generation card

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import { renderLvlPage } from './gameMunu.js'
+import { renderLvlPage } from './gameMunu'
 const levels = document.querySelectorAll('.radio')
 const start = document.querySelector('.start-btn')
 let userLvl = null
@@ -15,3 +15,33 @@ start.addEventListener('click', () => {
         renderLvlPage(userLvl)
     }
 })
+
+// событие при клике генерирует поле с нужным количеством карт
+
+start.addEventListener('click', () => {
+    if (userLvl === '1') {
+        renderLvlPage('6')
+    }
+    if (userLvl === '2') {
+        renderLvlPage('12')
+    }
+    if (userLvl === '3') {
+        renderLvlPage('18')
+    }
+})
+
+const flipCard = (e) => {
+    console.log('EVENT ON CARD CLICK', e.target.parentElement)
+}
+
+const cards = document.querySelectorAll('.memory-card')
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+})
+
+import { createGameMenu } from './gameMenu.js'
+const cardApp = () => {
+    createGameMenu
+}
+cardApp()
