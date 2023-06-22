f337c347bfd7eeece2036b0cfd91c72b7670e110
JulKuzmi <enchantress000@mail.ru>
2023-06-22 18:46:37 +0300

    choose level

--- src/gameMunu.js
+++ src/gameMunu.js
@@ -9,3 +9,35 @@ export function renderLvlPage(userLvl) {
     </div>`
     box.innerHTML = htmlGame
 }
+
+// export function startGame(difficult, cardsCount) {
+//     let firstCard = null;
+//     let secondCard = null;
+//     let open = '';
+
+// }
+
+export const createGameMenu = () => {
+    const title = document.createElement('h2')
+    const gameLevel = document.querySelector('level')
+
+    gameLevel.innerHTML = ''
+    title.textContent = 'Выберете сложность'
+    title.classList.add('game-menu_title')
+
+    const createDifficultButton = (difficult) => {
+        const button = document.createElement('button')
+
+        button.classList.add('game-menu_difficult-btn')
+        button.textContent = `${difficult} карт`
+
+        button.addEventListener('click', () => {})
+        return
+    }
+    gameLevel.append(
+        title,
+        createDifficultButton(6),
+        createDifficultButton(12),
+        createDifficultButton(18)
+    )
+}
