d82c22489c14197044f2e261f8d0f77800fe9846
JulKuzmi <enchantress000@mail.ru>
2023-07-09 13:48:24 +0300

    changes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
     entry: './src/script.ts',
+    mode: isProduction ? 'production' : 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         clean: true,
+        publicPath: '/',
     },
-
     module: {
         rules: [
             {
@@ -15,9 +22,36 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'src/css', to: 'static' }],
+        }),
+        new MiniCssExtractPlugin({
+            filename: './main.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: isProduction ? 'hidden-source-map' : 'source-map',
 }
